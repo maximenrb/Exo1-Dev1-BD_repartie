2dbea53d666ff5777310602c18faaf2cecd2fba0
adrienls <adrienls@riseup.net>
2020-10-09 20:15:21 +0200

    driver update

--- python_webCrawler/driverPath.py
+++ python_webCrawler/driverPath.py
@@ -12,6 +12,7 @@ absolute_path = absolute_path.replace("\\", "/")
 DRIVER_PATH = absolute_path + '/driver/chromedriver.exe'
 #DRIVER_PATH = absolute_path + '/driver/geckodriver.exe'
 #DRIVER_PATH = absolute_path + '/driver/chromedriver-83.0.4103.39-linux'
+#DRIVER_PATH = absolute_path + '/driver/geckodriver-v0.26-linux'
 
 def get_driver():
     # Change to '.Firefox()' if you use Firefox
