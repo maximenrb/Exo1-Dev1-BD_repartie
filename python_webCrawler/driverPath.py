2c59e621ac4e87e8d77f0b9544655c1fb311f5c5
Maxime <maxnarbaud@gmail.com>
2020-10-09 18:38:54 +0200

    Add chrome driver, and auto path search for web driver

--- python_webCrawler/driverPath.py
+++ python_webCrawler/driverPath.py
@@ -1,6 +1,11 @@
 # Must be changed :
 # https://www.selenium.dev/documentation/fr/getting_started_with_webdriver/third_party_drivers_and_plugins/
-DRIVER_PATH = 'python_webCrawler/data/geckodriver.exe'
+import pathlib
+
+absolute_path = str(pathlib.Path(__file__).parent.absolute())
+absolute_path = absolute_path.replace("\\", "/")
+
+DRIVER_PATH = absolute_path + '/driver/chromedriver.exe'
 
 
 def get_driver_path():
