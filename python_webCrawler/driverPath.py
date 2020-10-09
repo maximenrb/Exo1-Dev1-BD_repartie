6e635713a4e5008bcc766cb3934a38de9fca5634
Maxime <maxnarbaud@gmail.com>
2020-10-09 21:15:44 +0200

    Add choose os and browser feature for web driver

--- python_webCrawler/driverPath.py
+++ python_webCrawler/driverPath.py
@@ -1,20 +1,52 @@
 import pathlib
 from selenium import webdriver
 
-
+# Get absolute path
 absolute_path = str(pathlib.Path(__file__).parent.absolute())
 absolute_path = absolute_path.replace("\\", "/")
 
-# Must be changed :
+# Must be changed if you have some problems :
 # https://www.selenium.dev/documentation/fr/getting_started_with_webdriver/third_party_drivers_and_plugins/
-#
-# Change to '/driver/geckodriver.exe' if you want to use Firefox
-DRIVER_PATH = absolute_path + '/driver/chromedriver.exe'
-#DRIVER_PATH = absolute_path + '/driver/geckodriver.exe'
-#DRIVER_PATH = absolute_path + '/driver/chromedriver-83.0.4103.39-linux'
-#DRIVER_PATH = absolute_path + '/driver/geckodriver-v0.26-linux'
+DRIVER_PATH_WINDOWS_CHROME = absolute_path + '/driver/chromedriver.exe'
+DRIVER_PATH_WINDOWS_FIREFOX = absolute_path + '/driver/geckodriver.exe'
+
+DRIVER_PATH_LINUX_CHROME = absolute_path + '/driver/chromedriver-83.0.4103.39-linux'
+DRIVER_PATH_LINUX_FIREFOX = absolute_path + '/driver/geckodriver-v0.26-linux'
+
 
 def get_driver():
-    # Change to '.Firefox()' if you use Firefox
     # See more : https://www.selenium.dev/documentation/fr/webdriver/driver_requirements/
-    return webdriver.Chrome(executable_path=DRIVER_PATH)
+
+    user_os = ""
+    user_browser = ""
+
+    while not user_os == "1" and not user_os == "2":
+        print("Choose your OS:")
+        print("  1. Windows")
+        print("  2. Linux")
+
+        user_os = input("> ")
+        print("")
+
+    while not user_browser == "1" and not user_browser == "2":
+        print("Choose your browser:")
+        print("  1. Chrome")
+        print("  2. Firefox")
+
+        user_browser = input("> ")
+        print("")
+
+    while True:
+        if user_os == "1":
+            if user_browser == "1":
+                return webdriver.Chrome(executable_path=DRIVER_PATH_WINDOWS_CHROME)
+
+            elif user_browser == "2":
+                return webdriver.Firefox(executable_path=DRIVER_PATH_WINDOWS_FIREFOX)
+
+        elif user_os == "2":
+            if user_browser == "1":
+                return webdriver.Chrome(executable_path=DRIVER_PATH_LINUX_CHROME)
+
+            elif user_browser == 2:
+                return webdriver.Firefox(executable_path=DRIVER_PATH_LINUX_FIREFOX)
