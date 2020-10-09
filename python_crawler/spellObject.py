50ff1472b8f7069d1273a15a99c89ff6113838e1
Maxime <maxnarbaud@gmail.com>
2020-10-09 16:57:37 +0200

    Files deleted

--- python_crawler/spellObject.py
+++
@@ -1,6 +0,0 @@
-class SpellObject:
-    def __init__(self, name, level, components, spell_resistance):
-        self.name = name
-        self.level = level
-        self.components = components
-        self.spell_resistance = spell_resistance
