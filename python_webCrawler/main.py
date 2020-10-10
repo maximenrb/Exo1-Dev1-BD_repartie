0ba3a43f6085d1a0e498fab32d19d9cbfc02362b
adrienls <adrienls@riseup.net>
2020-10-10 17:33:11 +0200

    Execution time for the crawler

--- python_webCrawler/main.py
+++ python_webCrawler/main.py
@@ -2,7 +2,11 @@ from python_webCrawler.spellCrawler import multi_thread_crawler
 from python_webCrawler.spellCrawler import multi_process_crawler
 from python_webCrawler.spellCrawler import mono_thread_crawler
 from python_webCrawler.spellCrawler import home_crawler
+from time import time
 
-
+start = time()
 if __name__ == '__main__':
     mono_thread_crawler()
+
+end = time()
+print(str(end - start) + " s")
